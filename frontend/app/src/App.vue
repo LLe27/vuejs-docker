b7d5b2d5af3df152f26867ca4b605f86d6333d71
Long Le <lnle276@gmail.com>
2019-12-03 20:02:37 -0500

    set up the docker containers for the app, database and api

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-      hi there
+      Testing this app again
   </div>
 </template>
 
